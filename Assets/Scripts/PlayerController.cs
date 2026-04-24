19a5af0173cadb6c030b07889c62a52e9cb43824
gabeRam <gaberam310@gmail.com>
2026-04-24 00:43:22 -0700

    Added variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed = 5f;
+    private float gDirection = 0f; // Direction of gravity pull (1 for down, -1 for up)
+    public float pullStrength = 5f; // How strong the gravity pull is
+    
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0; // Disable gravity for the player
     }
 
     // Update is called once per frame
